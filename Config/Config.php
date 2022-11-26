269c8950860ef0e3de5eb8109cbdb4ba04e3692e
alberto.asow@gmail.com <alberto.asow@gmail.com>
2022-11-25 19:23:58 -0700

    base_url

--- Config/Config.php
+++ Config/Config.php
@@ -1,13 +1,14 @@
 <?php
 //Const para la base URL de la aplicaion¡
-const base_url = "http://localhost/PHP_projects/sistema_ventas_mvc/";
+//const base_url = "http://localhost/PHP_projects/sistema_ventas_mvc/"; //Variable para local
+const base_url = "https://sistema-ventas-production.up.railway.app/"; //Variable para produccion 
 
 
 //Constantes para la Conexion_control_Usuarios puerto 192.3.202.103:98
-const host_control_usuarios = "localhost";
-const user_control_usuarios = "root";
-const password_control_usuarios = "";
-const db_control_usuarios = "sistema_ventas";
+const host_control_usuarios = "localhost"; // Local host
+const user_control_usuarios = "root";// Local host
+const password_control_usuarios = "";// Local host
+const db_control_usuarios = "sistema_ventas";// Local host
 
 
 
